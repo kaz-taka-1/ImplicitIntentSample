adaca85ffbf2806759f8dad8a9d3079ecf9b4940
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-07-30 15:21:51 +0900

    地図検索ボタンタップ時の処理

--- app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.websarva.wings.android.implicitintentsample
 
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onMapSearchButtonClick(view: View){
+        val etSearchWord = findViewById<EditText>(R.id.etSearchWord)
+        var searchWord = etSearchWord.text.toString()
+        searchWord = URLEncoder.encode(searchWord,"UTF-8")
+        val uriStr = "geo:0,0?q=${searchWord}"
+        val uri = Uri.parse(uriStr)
+        val intent =Intent(Intent.ACTION_VIEW,uri)
+        startActivity(intent)
     }
 }
